7cfa6a792e4acb5b1411010f38a0eabcbf91422a
Renato Drumond <rm357140@fiap.com.br>
2025-03-25 15:05:50 -0300

    initial commit 25/03 _2

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,3 +1,9 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "arn:aws:s3:::urpdrum-us-east-1-terraform-action"  # Mantenha o ARN do seu bucket
+    key            = "terraform/state/dev.tfstate"  # Ajuste o caminho
+    region         = "us-east-1"  # **ADICIONE ISSO!**
+    encrypt        = true
+    dynamodb_table = "terraform-locks"  # Ajuste se estiver usando
+  }
 }
